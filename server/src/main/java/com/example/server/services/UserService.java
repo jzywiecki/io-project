9c73fe2a63121961998c6afb6f415f81488fa38e
bartipablo <bartek.pawlowski00@gmail.com>
2024-03-20 16:46:00 +0100

    added userRepository to userService

--- server/src/main/java/com/example/server/services/UserService.java
+++ server/src/main/java/com/example/server/services/UserService.java
@@ -1,9 +1,16 @@
 package com.example.server.services;
 
+import com.example.server.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
