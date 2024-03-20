c1b92abf5147ebbc18239b6db570bcf63aff256e
bartipablo <bartek.pawlowski00@gmail.com>
2024-03-21 00:38:12 +0100

    added endpoint: get room terms.

--- server/src/main/java/com/example/server/controllers/RoomController.java
+++ server/src/main/java/com/example/server/controllers/RoomController.java
@@ -1,7 +1,9 @@
 package com.example.server.controllers;
 
 import com.example.server.dto.RoomDto;
+import com.example.server.dto.TermDto;
 import com.example.server.services.RoomService;
+import com.example.server.services.TermService;
 import com.example.server.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,13 +22,31 @@ public class RoomController {
     private final RoomService roomService;
 
     /**
+     * Term service.
+     */
+    private final TermService termService;
+
+    /**
      * User service.
      */
     private final UserService userService;
 
+    /**
+     * Get all rooms to which the user is assigned.
+     * @return the rooms.
+     */
     @GetMapping("/get-user-rooms/{userId}")
     public ResponseEntity<List<RoomDto>> getUserRooms(@PathVariable Long userId) {
         return ResponseEntity.ok(userService.getUserRooms(userId));
+    }
+
+    /**
+     * Get all terms in the room
+     * @return the terms.
+     */
+    @GetMapping("/get-terms-in-room/{roomId}")
+    public ResponseEntity<List<TermDto>> getRoomTerms(@PathVariable Long roomId) {
+        return ResponseEntity.ok(termService.getRoomTerms(roomId));
     }
 
 }
--- server/src/main/java/com/example/server/services/TermService.java
+++ server/src/main/java/com/example/server/services/TermService.java
@@ -1,9 +1,48 @@
 package com.example.server.services;
 
+import com.example.server.dto.TermDto;
+import com.example.server.exceptions.RoomNotFoundException;
+import com.example.server.model.Room;
+import com.example.server.model.Term;
+import com.example.server.repositories.RoomRepository;
+import com.example.server.repositories.TermRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class TermService {
+
+    /**
+     * Room repository.
+     */
+    private final TermRepository termRepository;
+
+    /**
+     * Room repository.
+     */
+    private final RoomRepository roomRepository;
+
+    public List<TermDto> getRoomTerms(Long roomId) {
+        Room room = roomRepository
+                .findById(roomId)
+                .orElseThrow(
+                        () -> new RoomNotFoundException("Room with id: "
+                                + roomId
+                                + " not found.")
+                );
+
+        List<Term> terms = termRepository.findAllByRoomId(roomId);
+
+        return terms.stream()
+                .map((term) -> new TermDto(
+                        term.getDay(),
+                        term.getStartTime(),
+                        term.getEndTime()
+                ))
+                .collect(Collectors.toList());
+    }
 }
