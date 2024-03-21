d3a70cfd4e7bb583333be4b3177a9ce6b5c85ea2
Witold Strzeboński <wstrzebo@gmail.com>
2024-03-21 17:47:56 +0100

    Added term repository tests

--- server/src/main/java/com/example/server/services/RoomService.java
+++ server/src/main/java/com/example/server/services/RoomService.java
@@ -8,11 +8,8 @@ import com.example.server.model.Term;
 import com.example.server.repositories.RoomRepository;
 import com.example.server.repositories.TermRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
