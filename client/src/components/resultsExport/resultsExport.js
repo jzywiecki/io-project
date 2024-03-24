a03b3be141a7b4ac7d3e29bb0b010119810f6648
Kasl0 <slonieckacper@gmail.com>
2024-03-24 14:31:26 +0100

    Export results component

---
+++ client/src/components/resultsExport/resultsExport.js
@@ -0,0 +1,41 @@
+import "./resultsExport.css";
+import daysMap from '../common'
+
+function ResultsExport({results}) {
+
+    const CSVSeparator = ',';
+    const buttonTextContent = "Eksportuj do CSV";
+
+    const generateFileContent = () => {
+        const header = "imie,nazwisko,email,dzien,godzina\n";
+        const content = results
+            .map((result) => (
+                result.firstName + CSVSeparator +
+                result.lastName + CSVSeparator +
+                result.email + CSVSeparator +
+                daysMap[result.result.day] + CSVSeparator +
+                result.result.startTime + '-' + result.result.endTime
+            ));
+
+        return header + content.join('\n');
+    }
+
+    const handleExport = () => {
+        const file = new Blob([generateFileContent()], {type: 'text/plain'});
+        const element = document.createElement("a");
+        element.href = URL.createObjectURL(file);
+        element.download = "results.csv";
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+    };
+
+    return (
+        <div className="results-export">
+            <button onClick={handleExport} className="results-export-button">
+                {buttonTextContent}
+            </button>
+        </div>
+    );
+}
+export default ResultsExport;
