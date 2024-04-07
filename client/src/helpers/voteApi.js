735ed144fea58da0bf02e05562f836c6c7216352
bartipablo <bartek.pawlowski00@gmail.com>
2024-04-08 00:01:03 +0200

    fix: voteApi.js

--- client/src/helpers/voteApi.js
+++ client/src/helpers/voteApi.js
@@ -2,9 +2,9 @@ import axios from "axios"
 import { forEach } from "lodash"
 
 export const getVotingPage=(roomId, userId)=>{
-    return axios.get(`/api/get-voting-page/${roomId}/${userId}`)
+    return axios.get(`/api/vote/get-voting-page/${roomId}/${userId}`)
 }
 
 export const savePreferences = (roomId, userId, userPreferences) => {    
-    return axios.post(`/api/save-preferences/${roomId}/${userId}`, userPreferences) ;
+    return axios.post(`/api/vote/save-preferences/${roomId}/${userId}`, userPreferences) ;
 }
