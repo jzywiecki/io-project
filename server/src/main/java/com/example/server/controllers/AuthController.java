4b7cae7701e585bb5e72755f6037e41c30abe71d
Witold Strzeboński <wstrzebo@gmail.com>
2024-04-07 16:03:14 +0200

    Added account confirmation token

--- server/src/main/java/com/example/server/controllers/AuthController.java
+++ server/src/main/java/com/example/server/controllers/AuthController.java
@@ -1,9 +1,8 @@
 package com.example.server.controllers;
 
-import com.example.server.services.AuthService;
 import com.example.server.dto.LoginFormDto;
 import com.example.server.dto.UserDto;
-import com.example.server.model.User;
+import com.example.server.services.AuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,14 +15,18 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody UserDto userDto) {
-        User user = authService.register(userDto);
-        return ResponseEntity.ok(user);
+    public ResponseEntity<String> register(@RequestBody UserDto userDto) {
+        String token = authService.register(userDto);
+        return ResponseEntity.ok(token);
     }
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginFormDto loginFormDto) {
         String token = authService.login(loginFormDto.email(), loginFormDto.password());
         return ResponseEntity.ok(token);
+    }
+    @PostMapping("/email-confirmation/{token}")
+    public String confirmEmail(final @PathVariable String token) {
+        return authService.confirmEmail(token);
     }
 }
--- server/src/main/java/com/example/server/services/AuthService.java
+++ server/src/main/java/com/example/server/services/AuthService.java
@@ -8,6 +8,7 @@ import com.example.server.exceptions.UserNotRegisteredException;
 import com.example.server.exceptions.WrongPasswordException;
 import com.example.server.model.User;
 import com.example.server.repositories.UserRepository;
+import io.jsonwebtoken.ExpiredJwtException;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,7 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
 
-    public User register(UserDto userDto) {
+    public String register(UserDto userDto) {
         User user = userRepository.findByEmail(userDto.email())
                 .orElseThrow(() -> new UserNotFoundException(""));
 
@@ -31,10 +32,9 @@ public class AuthService {
         user.setLastName(userDto.lastName());
         user.setEmail(userDto.email());
         user.setPassword(passwordEncoder.encode(userDto.password()));
-        user.setActive(true);
         userRepository.save(user);
 
-        return user;
+        return jwtUtil.generateConfirmationToken(userDto.email());
     }
 
     public String login(String email, String password) {
@@ -49,6 +49,21 @@ public class AuthService {
             throw new WrongPasswordException();
         }
 
-        return jwtUtil.generateToken(email);
+        return jwtUtil.generateAccessToken(email);
+    }
+
+    public String confirmEmail(String token) {
+        String email;
+        try {
+            email = jwtUtil.extractEmailFromConfirmationToken(token);
+        } catch (ExpiredJwtException ex) {
+            return "Token expired";
+        }
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("user not found"));
+
+        user.setActive(true);
+        userRepository.save(user);
+        return "Email successfully confirmed";
     }
 }
