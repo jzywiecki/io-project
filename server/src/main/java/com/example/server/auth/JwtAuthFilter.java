11e75ad673e9044daeae41007efe348746c22ff2
Witold Strzeboński <wstrzebo@gmail.com>
2024-04-07 16:03:14 +0200

    Added account confirmation token

--- server/src/main/java/com/example/server/auth/JwtAuthFilter.java
+++ server/src/main/java/com/example/server/auth/JwtAuthFilter.java
@@ -19,21 +19,18 @@ import java.io.IOException;
 public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
     private final MyUserDetailsService myUserDetailsService;
-    private static final String TOKEN_HEADER = "Authorization";
-    private static final String TOKEN_PREFIX = "Bearer ";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String bearerToken = request.getHeader(TOKEN_HEADER);
-        if (bearerToken != null && bearerToken.startsWith(TOKEN_PREFIX)) {
-            String token = bearerToken.substring(TOKEN_PREFIX.length());
-            String email = jwtUtil.extractEmail(token);
-            if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetails userDetails = myUserDetailsService.loadUserByUsername(email);
-                Authentication authentication =
-                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            }
+        String token = jwtUtil.resolveToken(request);
+        if (token != null) {
+            String email = jwtUtil.extractEmailFromAccessToken(token);
+                if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    UserDetails userDetails = myUserDetailsService.loadUserByUsername(email);
+                    Authentication authentication =
+                            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
         }
         filterChain.doFilter(request, response);
     }
--- server/src/main/java/com/example/server/auth/JwtUtil.java
+++ server/src/main/java/com/example/server/auth/JwtUtil.java
@@ -3,32 +3,58 @@ package com.example.server.auth;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private static final String SECRET_KEY = "secretKey";
-    private static final long EXPIRATION_TIME = 1000 * 60 * 15;
+    private static final String ACCESS_SECRET_KEY = "accessSecretKey";
+    private static final String CONFIRMATION_SECRET_KEY = "confirmationSecretKey";
+    private static final long EXPIRATION_TIME = 1000 * 60 * 1;
+    private static final String TOKEN_HEADER = "Authorization";
+    private static final String TOKEN_PREFIX = "Bearer ";
 
-    private Claims extractAllClaims(String token) {
+    public String generateAccessToken(String email) {
+        return generateToken(email, ACCESS_SECRET_KEY);
+    }
+
+    public String generateConfirmationToken(String email) {
+        return generateToken(email, CONFIRMATION_SECRET_KEY);
+    }
+
+    public String extractEmailFromAccessToken(String token) {
+        return extractEmail(token, ACCESS_SECRET_KEY);
+    }
+
+    public String extractEmailFromConfirmationToken(String token) {
+        return extractEmail(token, CONFIRMATION_SECRET_KEY);
+    }
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(TOKEN_HEADER);
+        if (bearerToken != null && bearerToken.startsWith(TOKEN_PREFIX)) {
+            return bearerToken.substring(TOKEN_PREFIX.length());
+        }
+        return null;
+    }
+
+    private Claims extractAllClaims(String token, String secretKey) {
         return Jwts.parser()
-                .setSigningKey(SECRET_KEY)
+                .setSigningKey(secretKey)
                 .parseClaimsJws(token)
                 .getBody();
     }
 
-    public String extractEmail(String token) {
-        return extractAllClaims(token).getSubject();
+    private String extractEmail(String token, String secretKey) {
+        return extractAllClaims(token, secretKey).getSubject();
     }
 
-    public String generateToken(String email) {
-        System.out.println(new Date(System.currentTimeMillis() + EXPIRATION_TIME));
+    private String generateToken(String email, String secretKey) {
         return Jwts.builder()
                 .setSubject(email)
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .signWith(SignatureAlgorithm.HS512, secretKey)
                 .compact();
     }
 }
