af69014d3a2aad44bcf7ff88580fc4efa53dae29
Jakub Żywiecki <105950890+jzywiecki@users.noreply.github.com>
2024-04-07 20:40:18 +0200

    Update voteApi.js

--- client/src/helpers/voteApi.js
+++ client/src/helpers/voteApi.js
@@ -1,13 +1,10 @@
 import axios from "axios"
 import { forEach } from "lodash"
 
-const url="http://localhost:8080/api/vote"
-
-
 export const getVotingPage=(roomId, userId)=>{
-    return axios.get(url + `/get-voting-page/${roomId}/${userId}`)
+    return axios.get(`/get-voting-page/${roomId}/${userId}`)
 }
 
 export const savePreferences = (roomId, userId, userPreferences) => {    
-    return axios.post(url + `/save-preferences/${roomId}/${userId}`, userPreferences) ;
+    return axios.post(`/save-preferences/${roomId}/${userId}`, userPreferences) ;
 }
