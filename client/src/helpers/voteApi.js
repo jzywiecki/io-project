b4664c6fb3ad32f8e34b1ec47612ad29f029749b
jzywiecki <jakubzywiecki1@gmail.com>
2024-04-10 21:34:10 +0200

    Minor fix of vote api url (issue showing on aws)

--- client/src/helpers/voteApi.js
+++ client/src/helpers/voteApi.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { forEach } from "lodash"
 
 // const url="http://localhost:8080/api/vote"
-const url = "api/vote"
+const url = "/api/vote"
 
 export const getVotingPage=(roomId, userId)=>{
     return axios.get(url+`/get-voting-page/${roomId}/${userId}`)
