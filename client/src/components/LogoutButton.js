81a184723287b794fa2ef736394420fa5df219a1
wlpinjlte <wlpinjlte@gmail.com>
2024-04-14 23:33:31 +0200

    quick refactoring and cursor pointer added

--- client/src/components/LogoutButton.js
+++ client/src/components/LogoutButton.js
@@ -4,8 +4,11 @@ const LogoutButton=({className,...props})=>{
     const currentPath = window.location.pathname;
     const navigate=useNavigate()
     console.log(currentPath)
+    const onClickHandler=()=>{
+        localStorage.setItem("token","");navigate("/")
+    }
     return (<>
-    {currentPath!="/login"&&<i className={"fa fa-sign-out text-2xl "+className} onClick={()=>{localStorage.setItem("token","");navigate("/")}}></i>}
+    <i className={"fa fa-sign-out text-2xl cursor-pointer "+className} onClick={onClickHandler}></i>
     </>)
 }
 
